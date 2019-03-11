728a24d02e5dca93ae9bb274537aa2d4a5b364f8
mrc <marcoambrosini@pm.me>
2019-03-11 12:00:40 +0100

    initial commit

---
+++ src/styled-components/StyledPad.js
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+
+export default styled.button`
+  width: 80px;
+  height: 80px;
+  border-radius: 8px;
+  boder: 0px solid black;
+  box-shadow: 0px 0px 15px 0px #737373;
+  &:hover {
+    background-color: red;
+  }
+  &:active {
+    background-color: blue;
+  }
+  &:focus {
+    border: 0px solid black;
+  }
+`
