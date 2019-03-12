d29fc9e16a2fdf166f83e9902ff4ab49bba259b0
mrc <marcoambrosini@pm.me>
2019-03-12 16:56:22 +0100

    No lag between strokes

--- src/App.js
+++ src/App.js
@@ -1,46 +1,126 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+
 import StyledPad from "./styled-components/StyledPad"
 import Board from "./styled-components/Board"
 import DrumMachine from "./styled-components/DrumMachine"
 import Wrapper from "./styled-components/Wrapper"
 
 export default function App() {
+  useEffect(() => {
+    // This gets called after every render, by default
+    // (the first one, and every one after that)
+    document.addEventListener("keydown", e => {
+      if (
+        e.key === "q" ||
+        e.key === "w" ||
+        e.key === "e" ||
+        e.key === "a" ||
+        e.key === "s" ||
+        e.key === "d" ||
+        e.key === "z" ||
+        e.key === "x" ||
+        e.key === "c"
+      ) {
+        playSound(e.key)
+      }
+    })
+  })
+
   const handlePress = e => {
-    console.log(e.target.id)
-    let snd = new Audio("./assets/Korg-KR-55-mp3/55.bd1.mp3")
-    snd.play()
+    let padName = e.target.innerHTML[0]
+    playSound(padName)
+  }
+
+  const playSound = padName => {
+    let sound = document.getElementById(padName.toUpperCase())
+    sound.currentTime = 0
+    sound.play()
   }
 
   return (
     <Wrapper id="drum-machine">
       <DrumMachine id="display">
         <Board>
-          <StyledPad className="drum-pad" onClick={handlePress} id="Q">
+          <StyledPad className="drum-pad" onClick={handlePress}>
             q
+            <audio
+              preload="auto"
+              id="Q"
+              className="clip"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+            />
           </StyledPad>
-          <StyledPad className="drum-pad" onClick={handlePress} id="W">
+          <StyledPad className="drum-pad" onClick={handlePress}>
             w
+            <audio
+              preload="auto"
+              id="W"
+              className="clip"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+            />
           </StyledPad>
-          <StyledPad className="drum-pad" onClick={handlePress} id="E">
+          <StyledPad className="drum-pad" onClick={handlePress}>
             e
+            <audio
+              preload="auto"
+              id="E"
+              className="clip"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+            />
           </StyledPad>
-          <StyledPad className="drum-pad" onClick={handlePress} id="A">
+          <StyledPad className="drum-pad" onClick={handlePress}>
             a
+            <audio
+              preload="auto"
+              id="A"
+              className="clip"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+            />
           </StyledPad>
-          <StyledPad className="drum-pad" onClick={handlePress} id="S">
+          <StyledPad className="drum-pad" onClick={handlePress}>
             s
+            <audio
+              preload="auto"
+              id="S"
+              className="clip"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+            />
           </StyledPad>
-          <StyledPad className="drum-pad" onClick={handlePress} id="D">
+          <StyledPad className="drum-pad" onClick={handlePress}>
             d
+            <audio
+              preload="auto"
+              id="D"
+              className="clip"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+            />
           </StyledPad>
-          <StyledPad className="drum-pad" onClick={handlePress} id="Z">
+          <StyledPad className="drum-pad" onClick={handlePress}>
             z
+            <audio
+              preload="auto"
+              id="Z"
+              className="clip"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+            />
           </StyledPad>
-          <StyledPad className="drum-pad" onClick={handlePress} id="X">
+          <StyledPad className="drum-pad" onClick={handlePress}>
             x
+            <audio
+              preload="auto"
+              id="X"
+              className="clip"
+              src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+            />
           </StyledPad>
-          <StyledPad className="drum-pad" onClick={handlePress} id="C">
+          <StyledPad className="drum-pad" onClick={handlePress}>
             c
+            <audio
+              preload="auto"
+              id="C"
+              className="clip"
+              src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+            />
           </StyledPad>
         </Board>
       </DrumMachine>
